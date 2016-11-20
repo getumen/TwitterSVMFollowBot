f3cc69dc704c7794ca6a8185548ad350f8f8760b
getumen <n.yoshihiro.jp@gmail.com>
2016-11-21 00:50:48 +0900

    add print

--- stream.py
+++ stream.py
@@ -53,7 +53,7 @@ class StreamListener(tweepy.streaming.StreamListener):
     def on_status(self, status):
         self.cur.execute("INSERT INTO tweet VALUES (?,?,?,?,?,?)", self._parse_status(status))
         #self.cur.executemany("INSERT INTO word VALUES (?)", self._parse_text(status.text))
-
+        print(status.text.strip())
         self.count += 1
         if self.count % 100 == 0:
             self.conn.commit()
