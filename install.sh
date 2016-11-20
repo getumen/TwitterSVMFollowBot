6053d6e062e26531effd16bc95bb19a39f27171d
getumen <n.yoshihiro.jp@gmail.com>
2016-11-21 00:47:13 +0900

    fix typo

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ sudo apt-get install mecab mecab-ipadic-utf8
 sudo pip3 install -r requirements.txt
 
 # get from https://apps.twitter.com/
-# write the following in .bashrc
-TWITTER_CONSUMER_KEY='consumer_key'
-TWITTER_CONSUMER_SECRET='consumer_secret'
-TWITTER_ACCESS_KEY='access_key'
-TWITTER_ACCESS_SECRET='access_secret'
+export TWITTER_CONSUMER_KEY = 'consumer_key'
+export TWITTER_CONSUMER_SECRET = 'consumer_secret'
+export TWITTER_ACCESS_KEY = 'access_key'
+export TWITTER_ACCESS_SECRET = 'access_secret'
