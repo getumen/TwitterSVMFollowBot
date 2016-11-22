9ae14a74e5a4d4e90ca592f0687257bf4b89f839
getumen <n.yoshihiro.jp@gmail.com>
2016-11-22 12:32:33 +0900

    a

--- stream.py
+++ stream.py
@@ -56,7 +56,7 @@ class StreamListener(tweepy.streaming.StreamListener):
         self.cur.execute("REPLACE INTO user VALUES (?,?,?,?,?,?)", self._parse_status(status))
         # self.cur.executemany("INSERT INTO word VALUES (?)", self._parse_text(status.text))
         self.count += 1
-        print(self.count, status.text)
+        print(self.count, status.text.strip())
         if self.count % 100 == 0:
             self.conn.commit()
         if self.count % 1000 == 0:
